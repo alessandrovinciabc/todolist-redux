2c13b75434102fcbb36dff1a961a53d7c7fbcec0
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-07-01 17:34:21 +0200

    Create: slice and store for todo state

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    add(state, action) {
+      state.todos.push(action.item);
+    },
+    remove(state, action) {
+      state.todos = state.todos.filter((item) => item.id !== action.id);
+    },
+    toggle(state, action) {
+      let todoToChange = state.todos.find((el) => el.id === action.id);
+      todoToChange.done = !todoToChange.done;
+    },
+  },
+});
+
+export const { add, remove, toggle } = todoSlice.actions;
+
+export const selectTodos = (state) => state.todo.todos;
+
+export default todoSlice.reducer;
